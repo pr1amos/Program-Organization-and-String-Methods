2691666c8d1f165657d16bd8b2b0354396ce7a8e
pr1amos <157016419+pr1amos@users.noreply.github.com>
2024-05-25 16:13:43 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = ('В этом году я обучаюсь в Urban')
+print(my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string.upper()[29])
